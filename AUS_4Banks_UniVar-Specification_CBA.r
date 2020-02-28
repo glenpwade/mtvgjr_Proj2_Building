2d19c5ce558064c5cd59ca642a48437673f7d897
glenpwade <glenpwade@users.noreply.github.com>
2020-02-28 12:30:57 +1000

    latest

--- AUS_4Banks_UniVar-Specification_CBA.r
+++ AUS_4Banks_UniVar-Specification_CBA.r
@@ -3,9 +3,10 @@ gc(TRUE)
 
 ##====================== Initialisation ========================####
 if (TRUE){
-  #setwd("~/Annastiina/Topics/MTVGJR_MGARCH/Project2_Building")        # Anna's work PC  - GOOGLE DRIVE
-  setwd("~/Annastiina/Topics/MTVGJR_MGARCH/Project2_Building_GW")        # Glen's home PC - GOOGLE DRIVE
-  #setwd("D:/Source/Project2_Building")                                          # Anna's new Laptop
+  #setwd("~/Annastiina/Topics/MTVGJR_MGARCH/Project2_Building")        # Anna's work PC & Laptop - GOOGLE DRIVE
+  #setwd("~/Annastiina/Topics/MTVGJR_MGARCH/Project2_Building_GW")     # Glen's home PC - GOOGLE DRIVE
+  #setwd("D:/Source/Project2_Building")                                 # Anna's new Laptop
+  setwd("C:/Source/MTVGJR_MGARCH/Project2_Building")
   
   source("clsTV.r")
   source("clsGARCH.r")
@@ -28,10 +29,10 @@ if (TRUE){
   e_cba <- mydata$return_cba * 100 
   e_cba <- e_cba - mean(e_cba)
   e <- e_cba
-
+  
   # Plot the returns
   ptitle <- "CBA Returns"
-  plot(dates,e_cba,type="l",main=ptitle)
+  plot(dates,e,type="l",main=ptitle)
   
   # Read in the Garch'd Reference Data
   RefData <- readRDS("Gen_Refdata/RefData_withGarch_CBA_2020.RDS")
@@ -63,130 +64,124 @@ if (TRUE){
 # Plot returns using index as x-axis:
 ptitle <- "CBA Returns"
 plot(e,type="l",main = ptitle)
-abline(h=seq(-010,010,5), v=seq(0,7000,500),col="lightgrey")
+abline(h=seq(-10,10,5), v=seq(0,7000,500),col="lightgrey")
 lines(e)
 
 
-#### ========  SPECIFICATION - ORDER 3    ======== ####
-
-##==============================================================##
-##   TV - AUS_4Banks\CBA ---- ORDER 3  from 2018  ####
-##==============================================================##
-
-##  Method: Estimate the sub-sections of interest by changing the subset in the first line below
-##          Log the results for each test below the code.
-
-e <- e_cba[1:5400]
-e <- e_cba
-# Create a TV object with delta0, pars & st:
-ST <- 1:length(e)/length(e)
-startpars <- c(1,3,0.5,3,3,0.7,1,3,0.9)
-startpars <- c(1,2,0.2,3,3,0.5,3,3,0.8)
-TV <- newTV(st=ST,del0=1,vecpars=startpars,shape=c(TRshape$single,TRshape$single,TRshape$single),speedopt=TRspeedopt$eta)
-
-# Estimate the univar series:
-parScale <- c(1,1,1,0.3,3,1,0.3,3,1,0.3)
-nDeps <- rep(1e-5,length(parScale))
-# Estimate the univar series:
-nDeps <- c(1e-5,1e-5,1e-5,1e-9,1e-5,1e-5,1e-9,1e-5,1e-5,1e-9)  #LL = -8473.838 for [1:5400]
-nDeps <- c(1e-5,1e-5,1e-7,1e-9,1e-5,1e-7,1e-9,1e-5,1e-7,1e-9)  #LL = -8426.928 for [1:5400]
-
-TV$optimcontrol <- list(fnscale = -1,parscale=parScale,ndeps=nDeps,reltol=1e-7,maxit=1000)
-TV <- EstimateTV(e,TV)  # ,calcHess = TRUE,verbose = TRUE)
-# Estimation Outputs:
-TV$Estimated$value
-TV$Estimated$delta0
-TV$Estimated$pars
-plot(TV$Estimated$condvars,type = 'l',main=paste0("Loglik: ",round(TV$Estimated$value)))
-## Now calculate the standard errors for the model  ##
-TV <- calcStderr_TV(e,TV)
-TV$Estimated$stderr
-
-TV <- calcParamStats_TV(Tv)
-
-TV$Estimated$parsVector
-TV$Estimated$stderr
-TV$Estimated$tStat
-TV$Estimated$PValues
-
-saveRDS(TV,"Estimated_TVOrd3_CBA.RDS")
-
-TV <- readRDS("Estimated_TVOrd3_CBA.RDS")
-View(TV)
-
-if (!exists("refData")) refData <- readRDS("RefData_withGarch_CBA_2018.RDS")
-fName <- "TestStatDist_CBA.RDS"
-# Calculate the Reference test statistics:
-refTest <- list()
-refTest$LMTR2 <- myTest.TV.noGARCH.TR2(e,TV,TESTorder$H0)
-refTest$LMRobust <- myTest.TV.noGARCH.robust(e,TV,TESTorder$H0)
-TEST <- GenTestStatDist(e,TV,refData,refTest,saveas = fName,numloops = 1600)
-
-
-#### ========  SPECIFICATION - ORDER 3  - Results  ======== ##
 
 
 ##==============================================================##
-##  TV - AUS_4Banks\CBA ---- ORDER 3 ---- obs 1:7027            ##
-##
-## Result: Pvals=[TR2: %, Robust: %], using
-## TestStat_ProbDist_CBA Logliklihood Value:  -10817.27 - Can calc StdErrs
-## delta0: 1.310083
-## Pars: 
-##            [,1]       [,2]       [,3]
-##  delta -0.6138192 8.5151909 -8.085892
-##  speed  6.9999994 6.1432614  4.471730
-##  loc1   0.4215485 0.5952681  0.647414
-##  loc2          NA         NA         NA
-## TR2 - Cannot Reject the null: H0=Model is sufficient
-## Robust - Cannot Reject the null: H0=Model is sufficient
-#
-#
-# > TV$Estimated$parsVector
-# delta0    delta1    speed1    loc1.1    delta2    speed2    loc2.1    delta3    speed3    loc3.1 
-# 1.310083 -0.613819  6.999999  0.421548  8.515191  6.143261  0.595268 -8.085892  4.471730  0.647414 
-# > TV$Estimated$stderr
-# delta0   delta1   speed1   loc1.1   delta2   speed2   loc2.1   delta3   speed3   loc3.1 
-# 0.034062 0.045020 0.237471 0.000813 0.586107 0.115458 0.000908 0.588220 0.164732 0.000215 
-# > TV$Estimated$tStat
-# delta0     delta1     speed1     loc1.1     delta2     speed2     loc2.1     delta3     speed3     loc3.1 
-# 38.46172  -13.63436   29.47728  518.50923   14.52839   53.20775  655.58150  -13.74637   27.14549 3011.22791 
-# > TV$Estimated$PValues
-# delta0* delta1* speed1* loc1.1* delta2* speed2* loc2.1* delta3* speed3* loc3.1* 
-#   0       0       0       0       0       0       0       0       0       0 
-
-## I would recommend this model!
-## All estimates are significant, and LL is -10817  vs. -10778 for the Ord4 Model with some non-significant params 
-
-
+##  TV.ORDER 2, obs 1:7108  ####
 ##==============================================================##
-
-
-
-
-
-
-
-
-##==============================================================##
-##  TV.ORDER 2, MTVGJR Model  ####
-##==============================================================##
-
-## e = cba[1:7027]
-## delta0: 1.310083
-## Pars: 
-##            [,1]       [,2]       [,3]
-##  delta -0.6138192 8.5151909 -8.085892
-##  speed  6.9999994 6.1432614  4.471730
-##  loc1   0.4215485 0.5952681  0.647414
-## Logliklihood Value:  -10817.27 - Can calc StdErrs
-
-## Start pars:
-##  startpars <- c(1,3,0.5,3,3,0.7,1,3,0.9)
-##  startpars <- c(1,2,0.2,3,3,0.5,3,3,0.8)
 
 if (TRUE) {
   
+  e <- e_cba[1:7108]
+  nr.obs <- length(e)
+  st <- 1:nr.obs/nr.obs
+  
+  # Add some estimation controls
+  estCtrl <- list()
+  estCtrl$calcSE <- TRUE
+  estCtrl$verbose <- TRUE
+  
+  TV <- tv(st,c(tvshape$single,tvshape$double1loc))
+  
+  OptCtrl <- TV$optimcontrol
+  OptCtrl$reltol <- 1e-9
+  OptCtrl$ndeps <- c(1e-7,1e-7,1e-7,1e-7,1e-7,1e-7,1e-7)
+  #OptCtrl$parscale <- c(6,3,1,1,3,1,1)
+  OptCtrl$parscale <- c(3,3,1,1,3,1,1)
+  TV$optimcontrol <- OptCtrl
+  
+  TV$delta0 <- 1
+  TV$pars["deltaN",] <- c(1,1)
+  TV$pars["speedN",1] <- 3.05
+  TV$pars["locN1",1] <- 0.2
+  
+  TV$pars["speedN",2] <- 3.05
+  TV$pars["locN1",2] <- 0.6
+
+  TV <- estimateTV(e,TV,estCtrl)
+  
+  summary(TV)
+  plot(TV)
+  
+  # Fix the std errors
+  h0 <- TV$Estimated$hessian
+  h1 <- h0[-6,-6]
+  se1 <- try(stdErrors <- sqrt(-diag(qr.solve(h1))))
+  h2 <- h1[-3,-3]
+  se2 <- try(stdErrors <- sqrt(-diag(qr.solve(h2))))
+  
+  
+  # Now Test against CBA
+  TV <- setTaylorOrder(1,TV)
+  
+  RefTests <- list()
+  RefTests$LMTR2 <- LM.TR2(e,TV)
+  RefTests$LMRobust <- LM.Robust(e,TV)
+  
+  simcontrol <- list()
+  simcontrol$saveAs <- "CBA.ORD2.2_1-7108.RDS"
+  simcontrol$numLoops <- 120
+  simcontrol$numCores <- 3
+  TEST <- testStatDist(TV,RefData,RefTests,simcontrol)
+  
+  # View Distributions
+  hist(TEST$Stat_TR2,breaks = 15)
+  hist(TEST$Stat_Robust,breaks = 15)
+  
+}
+
+##==============================================================##
+
+# Estimation Results:
+#   
+#   Delta0 = 15.2539 se0 =  0.061529 
+# 
+#               st1      se1 sig        st2      se2   sig.1
+#   deltaN 0.433884 0.051305 *   -14.189119 0.067410   ***
+#   speedN 6.999964 0.182940 **    7.000000 0.000000   ***
+#   locN1  0.201900 0.001770 **    0.601815 0.001578   ***
+#   locN2        NA      NaN             NA      NaN      
+# 
+# Log-liklihood value:  -11794.82
+##==============================================================##
+
+
+##==============================================================##
+##  TV - AUS_4Banks/CBA ---- ---- obs 1:7108          
+## ORDER 2.1 
+## Result: Pvals=[TR2= 55%,Robust= 26%] 
+## ORDER 2.2
+## Result: Pvals=[TR2= 62%,Robust= 51%] 
+## ORDER 2.3
+## Result: Pvals=[TR2= %,Robust= %] 
+
+## Reject the null
+## Conclusion: No Evidence of another transition here
+##             Model is fully specified
+##==============================================================##
+
+
+## From 2018:
+##==============================================================##
+##  TV - AUS_4Banks\CBA ---- ORDER 3 ---- obs 1:7027            ##
+##
+## Result: Pvals=[TR2: 78.5%, Robust: 54.9%], using
+## TestStat_ProbDist_CBA Logliklihood Value:  -11438.12
+## delta0: 2.263294
+## Pars: 
+##            [,1]       [,2]       [,3]
+##  delta -1.3629961 23.1683258 -22.6972043
+##  speed  6.0774728  4.7347456   4.2034498
+##  loc1   0.1957340  0.6023369   0.6273989
+##  loc2   0.4019763         NA          NA
+##
+##==============================================================##
+
+if(TRUE){
   e <- e_cba
   nr.obs <- length(e)
   st <- 1:nr.obs/nr.obs
@@ -194,71 +189,40 @@ if (TRUE) {
   
   TV <- tv(st,c(tvshape$single,tvshape$single,tvshape$single))
   
-  OptCtrl <- TV$optimcontrol
-  OptCtrl$reltol <- 1e-7
+  OptCtrl$fnscale <- -1
+  OptCtrl$reltol <- 1e-9
+  OptCtrl$parscale <- c(3, 3,1,1, 3,3,1, 3,3,1)
+  OptCtrl$ndeps <- rep(1e-5, TV@nr.pars)
   #OptCtrl$ndeps <- rep(1e-5,TV@nr.pars)
-  OptCtrl$ndeps <- c(1e-5, 1e-5,1e-5,1e-5, 1e-5,1e-5,1e-5, 1e-5,1e-5,1e-5)
-  OptCtrl$parscale <- c(3, 1,1,0.3, 3,1,0.3, 3,1,0.3)
+  #OptCtrl$ndeps <- c(1e-5, 1e-5,1e-7,1e-5, 1e-7,1e-5,1e-5, 1e-7,1e-5,1e-5)
   TV$optimcontrol <- OptCtrl
   
-  TV$delta0 <- 1
-  TV$pars["deltaN",] <- c(1,3,1)
-  TV$pars["speedN",] <- c(3,3,3)
-  TV$pars["locN1",] <- c(0.2, 0.5, 0.8)
+  TV$delta0 <- 1.0
+  TV$pars["deltaN",] <- c(1, 3, -3)
+  TV$pars["speedN",] <- c(1, 3, 3)
+  TV$pars["locN1",] <- c(0.3, 0.6, 0.7)
+  #TV$pars["locN2",] <- c(0.3, NA, NA)
   
   TV <- estimateTV(e,TV,estCtrl)
   
   summary(TV)
   plot(TV)
-  
-}
 
-#  Univar TV  Estimation Results:
-#   
-#   Delta0 = 0 se0 =  NaN 
-# 
-#                st1      se1 sig      st2      se2 sig.1
-#   deltaN 1.2438307 0.060485 **  1.242096 0.030272   ** 
-#   speedN 4.6148744 0.130642 **  3.097853 0.167118   *  
-#   locN1  0.5580971 0.002868 **  0.893777 0.017572   ** 
-#   locN2         NA      NaN           NA      NaN      
-# 
-# Log-liklihood value:  -11512.76
+}
 
 # Estimation Results:
 #   
-#   Delta0 = 1.354377 se0 =  0.03582 
+#   Delta0 = 1.349635 se0 =  0.035672 
 # 
-#               st1      se1 sig       st2      se2 sig.1        st3      se3 sig.2
-#   deltaN -0.668492 0.049531 *   13.626951 3.941173   *   -13.183815 3.935306   *  
-#   speedN  6.879981 0.960068 *    4.799864 0.197317   **    4.289804 0.094964   ** 
-#   locN1   0.405107 0.002171 **   0.576614 0.004071   **    0.610202 0.006147   ** 
+# st1      se1 sig       st2      se2 sig.1        st3      se3 sig.2
+# deltaN -0.667736 0.048937 *   13.362309 3.673008   *   -12.917186 3.667844   *  
+#   speedN  6.999993 0.000000 ***  4.823184 0.194542   **    4.290551 0.095564   ** 
+#   locN1   0.404800 0.001835 ***  0.576191 0.003887   **    0.610496 0.006018   ** 
 #   locN2         NA      NaN            NA      NaN               NA      NaN      
 # 
-# Log-liklihood value:  -11046.54
+# Log-liklihood value:  -11035.09
 
-# Now Test: 
-if (TRUE) {
-  
-  # Read in the Garch'd Reference Data
-  RefData <- readRDS("Gen_Refdata/RefData_withGarch_CBA_2020.RDS")
-  
-  TV <- setTaylorOrder(2,TV)
-  
-  RefTests <- list()
-  RefTests$LMTR2 <- LM.TR2(e,TV)
-  RefTests$LMRobust <- LM.Robust(e,TV)
-  
-  simcontrol <- list()
-  simcontrol$saveAs <- "CBA"
-  simcontrol$numLoops <- 120
-  simcontrol$numCores <- 3
-  TEST <- testStatDist(TV,RefData,RefTests,simcontrol)
-  
-  # View Distributions
-  hist(TEST$Stat_TR2,breaks = 15)
-  hist(TEST$Stat_Robust,breaks = 15)  
-}
+
 
 
 ##==============================================================##
@@ -266,32 +230,32 @@ if (TRUE) {
 ##==============================================================##
 
 if (TRUE) {
-
-
+  
   # Create e, TV1 & G1 
   # make an original copy of the data:
-  e_orig <- e
+  e_orig <- e_cba
+  
   estCtrl <- list(calcSE = TRUE, verbose = TRUE)
   
   TV1 <- TV  # Estimated above
+  e <- e_orig/sqrt(TV1@g)
   
   G1 <- garch(garchtype$gjr)
   
   OptCtrl <- G1$optimcontrol
-  OptCtrl$reltol <- 1e-9
+  OptCtrl$reltol <- 1e-7
   OptCtrl$ndeps <- c(1e-5,1e-5,1e-5,1e-5)
   OptCtrl$parscale <- c(1,1,9,1)
   G1$optimcontrol <- OptCtrl
   
-  G1$pars["omega",1] <- 0.01
-  G1$pars["alpha",1] <- 0.02
+  G1$pars["omega",1] <- 0.02
+  G1$pars["alpha",1] <- 0.04
   G1$pars["beta",1] <- 0.8
   G1$pars["gamma",1] <- 0.03
   
   G1 <- estimateGARCH(e,G1,estCtrl)
   summary(G1)
   plot(G1)
-  
   
   MTV1 <- mtvgarch(e,TV1,G1)
   
@@ -300,30 +264,33 @@ if (TRUE) {
   # Filter out Garch & estimate TV component
   if(TRUE) {
     
-    #lG1 <- MTV1@garch                # For first iteration
-    lG1 <- MTV1$results[[1]]$garch   # For subsequent iterations - update the index!
+    lG1 <- MTV1@garch                # For first iteration
+    #lastResult <- length(MTV1$results)
+    #lG1 <- MTV1$results[[lastResult]]$garch   # For subsequent iterations 
     e <- e_orig/sqrt(lG1@h)
     
-    #lT1 <- MTV1@tv                # For first iteration
-    lT1 <- MTV1$results[[1]]$tv   # For subsequent iterations - update the index!
+    lT1 <- MTV1@tv                # For first iteration
+    #lT1 <- MTV1$results[[lastResult]]$tv   # For subsequent iterations 
     
     summary(lT1)
+    plot(lT1)
     ll.target <- getTargetValue(e,lT1)
     
     lT1$delta0 <- lT1$Estimated$delta0
     lT1$pars <- lT1$Estimated$pars 
     
-    lT1$pars["deltaN",] <- c(1,3,1)
-    lT1$pars["speedN",] <- c(3,3,3)
-    #lT1$pars["locN1",] <- c(0.1,0.4,0.5)
-    lT1$pars["locN1",] <- c(0.2,0.5,0.8)
+    lT1$pars["deltaN",] <- c(-1, 12, -11)
+    lT1$pars["speedN",] <- c(5.5, 3, 3)
+    lT1$pars["locN1",] <- c(0.3, 0.4, 0.5)
     
-    OptCtrl <- TV$optimcontrol
+    OptCtrl <- lT1$optimcontrol
     OptCtrl$reltol <- 1e-7
-    #OptCtrl$ndeps <- rep(1e-5,TV@nr.pars)
-    OptCtrl$ndeps <- c(1e-5, 1e-5,1e-5,1e-5, 1e-5,1e-5,1e-5, 1e-5,1e-5,1e-5)
-    OptCtrl$parscale <- c(3, 1,1,0.3, 3,1,0.3, 3,1,0.3)
-    TV$optimcontrol <- OptCtrl
+    #OptCtrl$parscale <- c(3,3,0.05, 5,3,0.05, 5,3,0.05)
+    OptCtrl$parscale <- c(1,3,1, 9,3,1, 9,3,1)
+    
+    #OptCtrl$ndeps <- rep(1e-5,lT1@nr.pars)
+    OptCtrl$ndeps <- c(1e-7,1e-7,1e-7, 1e-5,1e-7,1e-7, 1e-5,1e-7,1e-7)
+    lT1$optimcontrol <- OptCtrl
     
     lT1 <- estimateTV(e,lT1,estCtrl)
     summary(lT1)  
@@ -331,25 +298,26 @@ if (TRUE) {
     
     # Check that the LL is higher than the target:
     lT1$Estimated$value > ll.target 
-    
+  
   }
   
   # Filter out TV & estimate GARCH component
   if(TRUE) {
-    e <- e_orig/sqrt(lT1@g)
     
+    e <- e_orig/sqrt(lT1@g)
+
     summary(lG1)
     ll.target <- getTargetValue(e,garchObj = lG1)
     lG1$pars <- lG1$Estimated$pars
     
-    lG1$pars["omega",1] <- 0.02
-    lG1$pars["alpha",1] <- 0.03
-    lG1$pars["beta",1] <- 0.80
-    lG1$pars["gamma",1] <- 0.05
+    # lG1$pars["omega",1] <- 0.05
+    # lG1$pars["alpha",1] <- 0.05
+    # lG1$pars["beta",1] <- 0.80
+    # lG1$pars["gamma",1] <- 0.05
     
     optCtrl <- lG1$optimcontrol
-    optCtrl$reltol <- 1e-7
-    optCtrl$ndeps <- c(1e-5,1e-5,1e-7,1e-5)
+    optCtrl$reltol <- 1e-9
+    optCtrl$ndeps <- c(1e-7,1e-7,1e-7,1e-7)
     optCtrl$parscale <- c(1,1,9,1)
     lG1$optimcontrol <- optCtrl
     
@@ -367,24 +335,55 @@ if (TRUE) {
   MTV1 <- calcLoglik(MTV1,lT1,lG1)
   
   # Pick the best specification & write it to the Estimated List:
-  MTV1$Estimated[[1]] <- MTV1$results[[1]]
-  
+  MTV1$Estimated[[1]] <- MTV1$results[[2]]
   
   # Save the estimated model
   saveRDS(MTV1,"Output/CBA_mtvgjr_manual.RDS")
 }
 
-## Notes: Could not get a reliable estimate for TV in second round.
+
+
+
+
+
 
 ##==============================================================##
 ##                            THE END
 ##==============================================================##
 
-MTV1 <- readRDS("Output/CBA_mtvgjr_manual.RDS")
-summary(MTV1$results[[1]]$tv)
-plot(MTV1$results[[1]]$tv)
-summary(MTV1$results[[2]]$tv)
-plot(MTV1$results[[2]]$tv)
 
 
+# lT1 <- MTV1@tv                # For first iteration
+# #lT1 <- MTV1$results[[lastResult]]$tv   # For subsequent iterations 
+# 
+# summary(lT1)
+# plot(lT1)
+# ll.target <- getTargetValue(e,lT1)
+# 
+# lT1$delta0 <- lT1$Estimated$delta0
+# lT1$pars <- lT1$Estimated$pars 
+# 
+# lT1$pars["deltaN",] <- c(-1, 12, -11)
+# lT1$pars["speedN",] <- c(5.5, 3, 3)
+# lT1$pars["locN1",] <- c(0.3, 0.4, 0.5)
+# 
+# OptCtrl <- lT1$optimcontrol
+# OptCtrl$reltol <- 1e-7
+# #OptCtrl$parscale <- c(3,3,0.05, 5,3,0.05, 5,3,0.05)
+# OptCtrl$parscale <- c(1,3,1, 9,3,1, 9,3,1)
+# 
+# #OptCtrl$ndeps <- rep(1e-5,lT1@nr.pars)
+# OptCtrl$ndeps <- c(1e-7,1e-7,1e-7, 1e-5,1e-7,1e-7, 1e-5,1e-7,1e-7)
+# lT1$optimcontrol <- OptCtrl
 
+# Estimation Results:
+#   
+#   Delta0 = 1.349635 se0 =  NaN 
+# 
+# st1      se1 sig       st2      se2 sig.1        st3      se3 sig.2
+# deltaN -0.636138 0.034757 *   22.176415 4.649566   *   -21.793676 4.655014   *  
+#   speedN  6.774782 0.195077 **   4.578019 0.029249   **    4.309796 0.091418   ** 
+#   locN1   0.405353 0.002594 **   0.584026 0.002503   ***   0.604268 0.003920   ** 
+#   locN2         NA      NaN            NA      NaN               NA      NaN      
+# 
+# Log-liklihood value:  -11033.09
